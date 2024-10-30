49f69e3852604d1655fbbc72feb7e8304c68211f
Petar Dusevic <62700665+G-rox-y@users.noreply.github.com>
2024-10-30 21:02:24 +0100

    Integiranje frontenda
    Ubacio sam front u kod od projekta, sad sve radi zajedno

--- IzvorniKod/frontend/src/App.js
+++ IzvorniKod/frontend/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header';
+import Home from './Home';
+import Prijava from './Prijava';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './styles/app.css';
+import Registracija from './Registracija';
+import NapraviPonudu from './NapraviPonudu';
 
 function App() {
+  const kvartovi = [
+    {
+        id: 1,
+        image: "/images/header1.jpeg",
+        name: "Trešnjevka"
+    },
+    {
+        id: 2,
+        image: "/images/header1.jpeg",
+        name: "Špansko"
+    },
+    {
+        id: 3,
+        image: "/images/header1.jpeg",
+        name: "Dubrava"
+    }
+];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+            <Route path="/" element={<Home kvartovi={kvartovi}/>} />
+            <Route path="/napravi-ponudu" element={<NapraviPonudu />} />
+            <Route path="/ponude" element={<Prijava />} />
+            <Route path="/dogadaji" element={<Prijava />} />
+            <Route path="/prijava" element={<Prijava />} />
+            <Route path="/registracija" element={<Registracija />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
