0701280b7c62cc00f07f830036b33ad2a91dafdb
Petar Dusevic <pd55294@fer.hr>
2024-11-08 18:06:08 +0100

    Render Patch 6
    Ok, sad je trebalo dodati hosting link u allowed hosts

--- IzvorniKod/EjSused/settings.py
+++ IzvorniKod/EjSused/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#p*uuvv_ye90q704p^its-i2gvox70w_f-a(2f0oz+8t#b@1$5"
+SECRET_KEY = os.environ.get("SECRET_KEY", default="django-insecure-#p*uuvv_ye90q704p^its-i2gvox70w_f-a(2f0oz+8t#b@1$5")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -30,6 +30,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
