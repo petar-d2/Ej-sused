6e1d8e30dd46670eeb2e05b08b267fe4d76aafca
Tin Salopek <tin.salopek@fer.unizg.hr>
2025-01-20 14:23:29 +0100

    popravljen admin panel

--- IzvorniKod/frontend/src/AdminPrikaz.js
+++ IzvorniKod/frontend/src/AdminPrikaz.js
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import './styles/adminpanel.css';
 
 const AdminPrikaz = () => {
     const [susjeds, setSusjeds] = useState([]);
     const [tvrtkas, setTvrtkas] = useState([]);
     const [komentari, setKomentari] = useState([]);
-    const [loading, setLoading] = useState(true); // Loading state
+    const [loading, setLoading] = useState(true);
+    const [selectedSection, setSelectedSection] = useState('susjedi');  // State to track the selected section
     const navigate = useNavigate();
 
-    
     useEffect(() => {
         const user = JSON.parse(localStorage.getItem('user'));
-
         if (!user || !user.isNadlezna) {
             navigate('/unauthorized');
         }
     }, [navigate]);
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -29,9 +29,6 @@ const AdminPrikaz = () => {
                 setKomentari(komentariResponse.data);
             } catch (error) {
                 console.error('Error fetching data:', error.response || error.message || error);
-                console.error('Response data:', error.response.data); // Backend error message
-                console.error('Response status:', error.response.status); // HTTP status code
-                console.error('Response headers:', error.response.headers); // Headers info
             } finally {
                 setLoading(false);
             }
@@ -62,6 +59,10 @@ const AdminPrikaz = () => {
         }
     };
 
+    const handleSectionChange = (section) => {
+        setSelectedSection(section);
+    };
+
     if (loading) {
         return (
             <div className="loading-container">
@@ -72,63 +73,74 @@ const AdminPrikaz = () => {
 
     return (
         <div className="admin-prikaz">
-
-            <div className="section">
-                <h2>Susjedi</h2>
-                <div className="susjed-card-container">
-                    {susjeds.map((user) => (
-                        <div key={user.korisnik_id} className="susjed-card">
-                            <h3>{user.ime} {user.prezime}</h3>
-                            <button
-                                className="view-map-button"
-                                onClick={() => handleDeleteUser(user.korisnik_id, true)}
-                            >
-                                Delete
-                            </button>
-                        </div>
-                    ))}
-                </div>
+            <div className="section-selector">
+                <button className="view-map-button" onClick={() => handleSectionChange('susjedi')}>Susjedi</button>
+                <button className="view-map-button" onClick={() => handleSectionChange('tvrtke')}>Tvrtke</button>
+                <button className="view-map-button" onClick={() => handleSectionChange('komentari')}>Komentari</button>
             </div>
 
-            <div className="section">
-                <h2>Tvrtke</h2>
-                <div className="susjed-card-container">
-                    {tvrtkas.map((user) => (
-                        <div key={user.korisnik_id} className="susjed-card">
-                            <h3>{user.nazivTvrtka}</h3>
-                            <p>{user.opisTvrtka}</p>
-                            <button
-                                className="view-map-button"
-                                onClick={() => handleDeleteUser(user.korisnik_id, false)}
-                            >
-                                Delete
-                            </button>
+            
+                {selectedSection === 'susjedi' && (
+                    <div className="section">
+                        <h2>Susjedi</h2>
+                        <div className="susjed-card-container">
+                            {susjeds.map((user) => (
+                                <div key={user.korisnik_id} className="susjed-card">
+                                    <h3>{user.ime} {user.prezime}</h3>
+                                    <button
+                                        className="view-map-button"
+                                        onClick={() => handleDeleteUser(user.korisnik_id, true)}
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
-            </div>
+                    </div>
+                )}
 
-            <div className="section">
-                <h2>Komentari</h2>
-                <div className="susjed-card-container">
-                    {komentari.map((komentar) => (
-                        <div key={komentar.id} className="susjed-card">
-                            <p>{komentar.textKom}</p>
-                            <small><em>{komentar.sifDaje.email}</em></small>
-                            <button
-                                className="view-map-button"
-                                onClick={() => handleDeleteKomentar(komentar.sifKom)}
-                            >
-                                Delete
-                            </button>
+                {selectedSection === 'tvrtke' && (
+                    <div className="section">
+                        <h2>Tvrtke</h2>
+                        <div className="susjed-card-container">
+                            {tvrtkas.map((user) => (
+                                <div key={user.korisnik_id} className="susjed-card">
+                                    <h3>{user.nazivTvrtka}</h3>
+                                    <p>{user.opisTvrtka}</p>
+                                    <button
+                                        className="view-map-button"
+                                        onClick={() => handleDeleteUser(user.korisnik_id, false)}
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
-            </div>
+                    </div>
+                )}
+
+                {selectedSection === 'komentari' && (
+                    <div className="section">
+                        <h2>Komentari</h2>
+                        <div className="susjed-card-container">
+                            {komentari.map((komentar) => (
+                                <div key={komentar.id} className="susjed-card">
+                                    <p>{komentar.textKom}</p>
+                                    <small><em>{komentar.sifDaje.email}</em></small>
+                                    <button
+                                        className="view-map-button"
+                                        onClick={() => handleDeleteKomentar(komentar.sifKom)}
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                )}
+            
         </div>
     );
 };
 
 export default AdminPrikaz;
-
-
