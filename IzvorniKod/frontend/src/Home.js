479d2849013be093db81f6c6fc64bdb41b374d6d
RomanoRand <romano.randic@fer.hr>
2025-01-24 16:15:05 +0100

    mapa + nema vise imena stranice na mobitelu

--- IzvorniKod/frontend/src/Home.js
+++ IzvorniKod/frontend/src/Home.js
@@ -2,20 +2,35 @@ import React, { useContext } from 'react';
 import './styles/home.css';
 import PrikazKvarta from './PrikazKvarta';
 import { GlobalContext } from './GlobalContext';
+import Mapa from './Mapa';
 
 const Home = () => {
+  const predefinedLocations = {
+    'Trešnjevka': [45.796, 15.945],
+    'Maksimir': [45.829, 16.018],
+    'Dubrava': [45.817, 16.067],
+    'Žitnjak': [45.794, 16.058],
+    'Trnje': [45.801, 15.974],
+    'Črnomerec': [45.821, 15.933],
+    'Vrapče': [45.837, 15.910],
+    'Novi Zagreb': [45.770, 15.977],
+    'Medveščak': [45.818, 15.988],
+  };
+  const { kvartovi } = useContext(GlobalContext);
 
-    const { kvartovi } = useContext(GlobalContext);
-
-    return(
-        <div className="home_main">
-            {kvartovi.map((kvart) => (
-                <PrikazKvarta 
-                    key={kvart.id}
-                    kvart={kvart} />
-            ))}
+  return (
+    <div className="home_main">
+        <div className='mapa'>
+        <Mapa predefinedLocations={predefinedLocations} />
         </div>
-    );
-}
+        <div className='kvartovi_home'>
+            {kvartovi.map((kvart) => (
+            <PrikazKvarta key={kvart.id} kvart={kvart} />
+        
+      ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- IzvorniKod/frontend/src/Mapa.js
+++ IzvorniKod/frontend/src/Mapa.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-
 import L from "leaflet";
 import "leaflet.awesome-markers/dist/leaflet.awesome-markers";
 import "leaflet.awesome-markers/dist/leaflet.awesome-markers.css";
@@ -12,17 +11,18 @@ const icon = L.AwesomeMarkers.icon({
   prefix: "glyphicon",
 });
 
-
-const Mapa = ({ lokacija = [45.33, 14.46], zoom = 13, naslov = "Dana lokacija" }) => {
+const Mapa = ({ predefinedLocations = {}, zoom = 13 }) => {
   return (
-    <MapContainer center={lokacija} zoom={zoom} style={{ height: "100%", width: "100%" }}>
+    <MapContainer center={[45.796, 15.945]} zoom={zoom} style={{ height: "100%", width: "100%" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={lokacija} icon={icon}>
-        <Popup>{naslov}</Popup>
-      </Marker>
+      {Object.keys(predefinedLocations).map((key) => (
+        <Marker key={key} position={predefinedLocations[key]} icon={icon}>
+          <Popup>{key}</Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
